a75f589ae404ddc6ebf35743d2bff11aa788fe09
Stefan VanBuren <vanburenstefan@gmail.com>
2018-05-27 12:35:00 -0400

    Initial commit

---
+++ merkle_tree.py
@@ -0,0 +1,49 @@
+import attr
+
+
+@attr.s
+class BinaryMerkleTree:
+    data = attr.ib(default=None)
+    _left = attr.ib(default=None, init=False)
+    _right = attr.ib(default=None, init=False)
+    _hash = attr.ib(init=False)
+
+    @data.validator
+    def sortable(self, attribute, value):
+        cls = value.__class__
+        if cls.__lt__ == object.__lt__ and cls.__gt__ == object.__gt__:
+            raise ValueError("Need comparable object for data")
+
+    @_hash.default
+    def calculate_hash(self):
+        h = hash(self.data)
+        if self._left:
+            h += self._left.calculate_hash()
+        if self._right:
+            h += self._right.calculate_hash()
+        return h
+
+    def get_hash(self):
+        return self._hash
+
+    def insert(self, data):
+        if not isinstance(self.data, data.__class__):
+            raise ValueError("Type of data must be the same")
+        if data < self.data:
+            if self._left:
+                self._left.insert(data)
+            else:
+                self._left = BinaryMerkleTree(data)
+        else:
+            if self._right:
+                self._right.insert(data)
+            else:
+                self._right = BinaryMerkleTree(data)
+        self._hash = self.calculate_hash()
+
+    def print(self):
+        if self._left:
+            self._left.print()
+        print(f"data: {self.data}, hash: {self._hash}")
+        if self._right:
+            self._right.print()
